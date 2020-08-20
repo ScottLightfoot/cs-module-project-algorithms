1f5e759fa405c52d718038d7f2b68585d42647d2
ScottLightfoot <gslightfoot@gmail.com>
2020-08-19 22:59:51 -0500

    rps complete with tests passing

--- 06_rock_paper_scissors/rps.py
+++ 06_rock_paper_scissors/rps.py
@@ -2,15 +2,25 @@
 
 import sys
 
+outcomes = ['rock'], ['paper'], ['scissors']
+
+def add_layer(curr_list):
+    return [i + j for i in curr_list for j in outcomes]
+
 def rock_paper_scissors(n):
-  # Your code here
-
-  pass
-
+    if n == 0:
+        return [[]]
+    else:
+        # cartesian product?
+        ret_list = [i for i in outcomes]
+        while n > 1:
+            ret_list = add_layer(ret_list)
+            n -= 1
+        return ret_list
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
